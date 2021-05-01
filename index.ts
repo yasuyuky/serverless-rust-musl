791f8f46f842da61a34dfb40475862257aeea566
yasuyuky <yasuyuki.ymd@gmail.com>
2021-05-01 23:30:08 +0900

    Add the feature to add default deps

--- index.ts
+++ index.ts
@@ -8,18 +8,25 @@ class RustMusl implements Plugin {
   hooks: Plugin.Hooks;
   commands: Plugin.Commands;
 
+  defaultDependencies: [string, string[]][] = [
+    ["lambda_runtime", []],
+    ["tokio", ["full"]],
+    ["openssl", ["vendored"]],
+  ];
+
   constructor(serverless: Serverless, options: Serverless.Options) {
     this.serverless = serverless;
     this.options = options;
 
     this.commands = {
       cargoinit: {
-        lifecycleEvents: ["init"],
+        lifecycleEvents: ["init", "addDependencies"],
       },
     };
 
     this.hooks = {
       "cargoinit:init": this.init.bind(this),
+      "cargoinit:addDependencies": this.addDependencies.bind(this),
     };
   }
 
@@ -32,6 +39,16 @@ class RustMusl implements Plugin {
   init() {
     if (!this.check()) return;
     spawnSync("cargo", ["init"]);
+  }
+
+  addDependencies() {
+    for (let dep of this.defaultDependencies) {
+      console.log(dep);
+      let args = ["add", dep[0]].concat(
+        dep[1].length ? ["--features"].concat(dep[1]) : []
+      );
+      spawnSync("cargo", args);
+    }
   }
 }
 

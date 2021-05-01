f180e47dcb486c81fe0e7b53c5635c6ebd9b7456
yasuyuky <yasuyuki.ymd@gmail.com>
2021-05-01 23:30:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var child_process_1 = require("child_process");
 var RustMusl = /** @class */ (function () {
     function RustMusl(serverless, options) {
+        this.defaultDependencies = [
+            ["lambda_runtime", []],
+            ["tokio", ["full"]],
+            ["openssl", ["vendored"]],
+        ];
         this.serverless = serverless;
         this.options = options;
         this.commands = {
             cargoinit: {
-                lifecycleEvents: ["init"],
+                lifecycleEvents: ["init", "addDependencies"],
             },
         };
         this.hooks = {
             "cargoinit:init": this.init.bind(this),
+            "cargoinit:addDependencies": this.addDependencies.bind(this),
         };
     }
     RustMusl.prototype.check = function () {
@@ -25,6 +31,14 @@ var RustMusl = /** @class */ (function () {
         if (!this.check())
             return;
         child_process_1.spawnSync("cargo", ["init"]);
+    };
+    RustMusl.prototype.addDependencies = function () {
+        for (var _i = 0, _a = this.defaultDependencies; _i < _a.length; _i++) {
+            var dep = _a[_i];
+            console.log(dep);
+            var args = ["add", dep[0]].concat(dep[1].length ? ["--features"].concat(dep[1]) : []);
+            child_process_1.spawnSync("cargo", args);
+        }
     };
     return RustMusl;
 }());
